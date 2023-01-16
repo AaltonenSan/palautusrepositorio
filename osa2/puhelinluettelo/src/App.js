7a391d1af77995421519ca57cdd78de80983dd28
AaltonenSan <santtu.36@gmail.com>
2023-01-16 16:59:00 +0200

    step6

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Filter = (props) => {
   return (
@@ -30,18 +31,15 @@ const OnePerson = (props) => {
 const Persons = ({persons}) => {
   return (
     <div>
-      {persons.map(person => <OnePerson key={person.name} name={person.name} number={person.number} />)}
+      {persons.map(person =>
+        <OnePerson key={person.name} name={person.name} number={person.number} />
+      )}
     </div>
   )
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [persons, setPersons] = useState([])
   const [newPerson, setNewPerson] = useState(
     {
       name: '',
@@ -49,6 +47,14 @@ const App = () => {
     }
   )
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleChangePerson = (e) => {
     setNewPerson({ ...newPerson, [e.target.name]: e.target.value })
