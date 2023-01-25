4aa2fc40eb7d6359d297b2c8d6120b5cecbffb48
AaltonenSan <santtu.36@gmail.com>
2023-01-25 21:14:49 +0200

    Osat 2.11 - 2.17

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+import { useEffect, useState } from 'react';
+import contactService from './services/contacts';
 
 const Filter = (props) => {
   return (
@@ -7,7 +7,7 @@ const Filter = (props) => {
       filter shown with <input type="text" value={props.filter} onChange={props.onChange} />
     </div>
   )
-}
+};
 const PersonForm = (props) => {
   return (
     <form>
@@ -22,79 +22,153 @@ const PersonForm = (props) => {
       </div>
     </form>
   )
-}
-const OnePerson = (props) => {
-  return (
-    <p>{props.name} {props.number}</p>
-  )
-}
-const Persons = ({persons}) => {
+};
+const Persons = ({ persons, setPersons, setMessage }) => {
+  const deleteContact = removePerson => {
+    if (window.confirm(`Delete ${removePerson.name} ?`)) {
+      contactService
+        .deleteContact(removePerson.id)
+        .then(() => {
+          setPersons(persons.filter(person => person.name !== removePerson.name))
+          setMessage(`Deleted ${removePerson.name}`)
+          setTimeout(() => {
+            setMessage(null)
+          }, 5000)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  };
   return (
     <div>
       {persons.map(person =>
-        <OnePerson key={person.name} name={person.name} number={person.number} />
+        <div key={person.name} style={{ display: 'flex', alignItems: 'center' }}>
+          <p>{person.name} {person.number}</p>
+          <button onClick={() => deleteContact(person)}>delete</button>
+        </div>
       )}
+    </div>
+  )
+};
+
+const Notification = ({ message, error }) => {
+  const notificationStyle = {
+    color: message ? 'green' : 'red',
+    background: 'lightgrey',
+    fontSize: 20,
+    borderStyle: 'solid',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10
+  }
+
+  if (message === null && error === null) {
+    return null
+  }
+  return (
+    <div style={notificationStyle}>
+      {message || error}
     </div>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([])
-  const [newPerson, setNewPerson] = useState(
-    {
-      name: '',
-      number: ''
-    }
-  )
   const [filter, setFilter] = useState('')
+  const [newPerson, setNewPerson] = useState({ name: '', number: '' })
+  const [message, setMessage] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
+    contactService
+      .getAll()
+      .then(initialContacts => {
+        setPersons(initialContacts)
       })
-  }, [])
+      .catch(error => {
+        console.log(error)
+        setErrorMessage("Can't get contacts from server")
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+      })
+  }, []);
 
-  const handleChangePerson = (e) => {
+  const handleChangePerson = e => {
     setNewPerson({ ...newPerson, [e.target.name]: e.target.value })
-  }
-  const handleChangeFilter = (e) => {
-    setFilter(e.target.value)
-  }
+  };
 
-  const filterPhonebook = (phonebook) => {
-    return phonebook.filter(
-      person =>
-        person.name.toLowerCase().includes(filter.toLowerCase())
+  const filterPhonebook = phonebook => {
+    return phonebook.filter(person =>
+      person.name.toLowerCase().includes(filter.toLowerCase())
     )
+  };
+
+  const updatePerson = () => {
+    if (window.confirm(`${newPerson.name} is already added to phonebook, replace the old number with a new one?`)) {
+      const person = persons.find(p => p.name === newPerson.name);
+      const changedPerson = { ...person, number: newPerson.number };
+      contactService
+        .updateContact(person.id, changedPerson)
+        .then(returnedPerson => {
+          setPersons(persons.map(person => person.id !== returnedPerson.id ? person : returnedPerson))
+          setMessage(`Updated ${newPerson.name}`)
+          setTimeout(() => {
+            setMessage(null)
+          }, 5000)
+        })
+        .catch(error => {
+          console.log(error)
+          setErrorMessage(`Information of ${newPerson.name} has already been removed from server`)
+          setTimeout(() => {
+            setErrorMessage(null)
+          }, 5000)
+          setPersons(persons.filter(person => person.name !== newPerson.name))
+        })
+      setNewPerson({name: '', number: ''})
+    }
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault()
     if (persons.some(person => person.name === newPerson.name)) {
-      alert(`${newPerson.name} is already added to phonebook`)
+      updatePerson();
       return;
     }
-    setPersons(
-      [...persons, { name: newPerson.name, number: newPerson.number }]
-    )
-    setNewPerson(
-      {
-        name: '',
-        number: ''
-      }
-    )
-  }
+    contactService
+      .createContact(newPerson)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+        setMessage(`Added ${newPerson.name}`)
+        setTimeout(() => {
+          setMessage(null)
+        }, 5000)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    setNewPerson({name: '', number: ''})
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter filter={filter} onChange={(e) => handleChangeFilter(e)} />
-      <h2>add a new</h2>
-      <PersonForm name={newPerson.name} number={newPerson.number} onChange={(e) => handleChangePerson(e)} handleSubmit={handleSubmit} />
-      <h2>Numbers</h2>
-      <Persons persons={filterPhonebook(persons)} />
+      <Notification message={message} error={errorMessage} />
+      <Filter filter={filter} onChange={e => setFilter(e.target.value)} />
+      <h3>add a new</h3>
+      <PersonForm
+        name={newPerson.name}
+        number={newPerson.number}
+        onChange={e => handleChangePerson(e)}
+        handleSubmit={handleSubmit}
+      />
+      <h3>Numbers</h3>
+      <Persons
+        persons={filterPhonebook(persons)}
+        setPersons={setPersons}
+        setMessage={setMessage}
+      />
     </div>
   )
 
